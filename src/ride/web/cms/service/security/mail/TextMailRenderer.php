4c5eec5150ad91ca64aaeca45f3069331e6e4191
Kaya <kayalion@kayalion.net>
2018-03-21 12:09:50 +0100

    opened up processing of variables

--- src/ride/web/cms/service/security/mail/TextMailRenderer.php
+++ src/ride/web/cms/service/security/mail/TextMailRenderer.php
@@ -8,13 +8,13 @@ use \ride\web\base\service\security\mail\TextMailRenderer as BaseTextMailRendere
  * Text mail renderer for the CMS
  */
 class TextMailRenderer extends BaseTextMailRenderer {
-    
+
     /**
      * Value for the URL variable
      * @var string
      */
     protected $url;
-    
+
     /**
      * Sets the URL variable
      * @param string $url
@@ -23,18 +23,18 @@ class TextMailRenderer extends BaseTextMailRenderer {
     public function setUrl($url) {
         $this->url = $url;
     }
-    
+
     /**
      * Hook to process the variables before rendering the template
      * @param array $variables Variables to be passed to the template
      * @return array Process variables to be passed to the template
      */
-    protected function processVariables(array $variables) {
+    public function processVariables(array $variables) {
         $variables = parent::processVariables($variables);
-        
+
         if ($this->url) {
             $url = $this->url;
-            
+
             if (isset($variables['encryptedUsername'])) {
                 $url = str_replace('%25user%25', $variables['encryptedUsername'], $url);
             }
@@ -44,17 +44,17 @@ class TextMailRenderer extends BaseTextMailRenderer {
             if (isset($variables['encryptedTime'])) {
                 $url = str_replace('%25time%25', $variables['encryptedTime'], $url);
             }
-            
+
             $variables['url'] = $url;
         }
-        
+
         if (isset($variables['user'])) {
             $variables['username'] = $variables['user']->getUserName();
             $variables['name'] = $variables['user']->getDisplayName();
             $variables['email'] = $variables['user']->getEmail();
         }
-        
+
         return $variables;
     }
-    
+
 }
